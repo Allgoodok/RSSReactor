5e8a458ac655dfb579409fb81a1d20ffc298f65f
Vlad Gudikov <vgoodko@gmail.com>
2016-03-15 20:42:06 +0200

    Initial commit

---
+++ app/src/main/java/com/android/internship/rssreactor/FeedsActivity.java
@@ -0,0 +1,13 @@
+package com.android.internship.rssreactor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FeedsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feeds);
+    }
+}
